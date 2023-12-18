7b0eedb4b166403cae6a9044fdb31c5fb137c9df
40758 <40758@DESKTOP-U6HJMHG>
2023-12-18 17:30:01 +0200

    Add project files.

---
+++ Sinca_Teodora_Lab11/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Sinca_Teodora_Lab11.Data;
+
+namespace Sinca_Teodora_Lab11
+{
+    public partial class App : Application
+    {
+        public static ShoppingListDatabase Database { get; private set; }
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+            Database = new ShoppingListDatabase(new RestService());
+            MainPage = new NavigationPage(new ListEntryPage());
+        }
+    }
+}
---
+++ Sinca_Teodora_Lab11/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Sinca_Teodora_Lab11
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
